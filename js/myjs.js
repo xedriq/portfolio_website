f3a894bafb3dcc81c0c01203cd20c744dcef0fff
xedriq <xedriq@gmail.com>
2019-05-26 12:37:15 +0800

    finished home page and animations

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        items: 1,
+        loop: true,
+        margin: 0,
+        stagePadding: 0,
+        autoplay: true,
+        autoplayTimeout: 6000,
+    });
+  });
