71aad9ff90b896450f733832f7d911695c59df3d
xedriq <xedriq@gmail.com>
2019-06-03 22:03:02 +0800

    added scroll effects

--- js/myjs.js
+++ js/myjs.js
@@ -14,4 +14,26 @@ $(document).ready(function () {
   $(".navigation__link").click(function () {
     $("#navi_toggle").prop("checked", false);
   });
+
+  var scrollLink = $('.scroll');
+  //Smooth Scrolling
+  scrollLink.click(function (e) {
+    e.preventDefault();
+    $('body,html').stop().animate({
+      scrollTop: $(this.hash).offset().top - 150
+    }, 800);
+  });
+
+  $(window).scroll(function () {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function () {
+      var sectionOffset = $(this.hash).offset().top-210;
+
+      if (sectionOffset <= scrollbarLocation) {
+        $(this).addClass('navigation__link--active');
+        $(this).parents().siblings().children().removeClass('navigation__link--active');
+      }
+    })
+  })
 });
